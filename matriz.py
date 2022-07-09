33a34be4a93395cf96ce3c5acd445b433df7bf7a
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2022-07-08 21:34:45 -0300

    refatorando tuplas de cores

--- matriz.py
+++ matriz.py
@@ -21,26 +21,34 @@ def re_create():
     glClearColor(0.4, 0.4, 0.4, 1.0)  # cor de fundo
     glClear(GL_COLOR_BUFFER_BIT)
 
+    colorStench = (0.75, 0.56, 0)
+    colorWumpus =(0.47, 0.24, 0.015)
+    coloragent = (0.5, 0.5, 0.5)
+    colorWhite = (1, 1, 1)
+    colorGSB = (0.83, 0.91, 0.82)
+    colorBreeze = (0.64, 0.76, 0.95)
+    colorPit = (0, 0, 0)
+
     # primeira coluna
-    create_square((0.75, 0.56, 0), ((1, 1), (6, 1), (6, 6), (1, 6)))  # amarelo Stench
-    create_square((0.47, 0.24, 0.015), ((1, 7), (6, 7), (6, 12), (1, 12)))  # Wumpus
-    create_square((0.75, 0.56, 0), ((1, 13), (6, 13), (6, 18), (1, 18)))  # amarelo Stench
-    create_square((0.5, 0.5, 0.5), ((1, 19), (6, 19), (6, 24), (1, 24)))  # Cinza Agent
+    create_square((colorStench), ((1, 1), (6, 1), (6, 6), (1, 6)))  # amarelo Stench
+    create_square((colorWumpus), ((1, 7), (6, 7), (6, 12), (1, 12)))  # Wumpus
+    create_square((colorStench), ((1, 13), (6, 13), (6, 18), (1, 18)))  # amarelo Stench
+    create_square((coloragent), ((1, 19), (6, 19), (6, 24), (1, 24)))  # Cinza Agent
     # segunda coluna
-    create_square((1, 1, 1), ((7, 1), (12, 1), (12, 6), (7, 6)))  # Branco
-    create_square((0.83, 0.91, 0.82), ((7, 7), (12, 7), (12, 12), (7, 12)))  # Ouro, Stench, Breeze
-    create_square((1, 1, 1), ((7, 13), (12, 13), (12, 18), (7, 18)))  # Branco
-    create_square((0.64, 0.76, 0.95), ((7, 19), (12, 19), (12, 24), (7, 24)))  # Azul breeze
+    create_square((colorWhite), ((7, 1), (12, 1), (12, 6), (7, 6)))  # Branco
+    create_square((colorGSB), ((7, 7), (12, 7), (12, 12), (7, 12)))  # Ouro, Stench, Breeze
+    create_square((colorWhite), ((7, 13), (12, 13), (12, 18), (7, 18)))  # Branco
+    create_square((colorBreeze), ((7, 19), (12, 19), (12, 24), (7, 24)))  # Azul breeze
     # terceira coluna
-    create_square((0.64, 0.76, 0.95), ((13, 1), (18, 1), (18, 6), (13, 6)))  # Azul Breeze
-    create_square((0, 0, 0), ((13, 7), (18, 7), (18, 12), (13, 12)))  # preto pit
-    create_square((0.64, 0.76, 0.95), ((13, 13), (18, 13), (18, 18), (13, 18)))  # Azul Breeze
-    create_square((0, 0, 0), ((13, 19), (18, 19), (18, 24), (13, 24)))  # Preto Pit
+    create_square((colorBreeze), ((13, 1), (18, 1), (18, 6), (13, 6)))  # Azul Breeze
+    create_square((colorPit), ((13, 7), (18, 7), (18, 12), (13, 12)))  # preto pit
+    create_square((colorBreeze), ((13, 13), (18, 13), (18, 18), (13, 18)))  # Azul Breeze
+    create_square((colorPit), ((13, 19), (18, 19), (18, 24), (13, 24)))  # Preto Pit
     # Quarta coluna
-    create_square((0, 0, 0), ((19, 1), (24, 1), (24, 6), (19, 6)))  # Preto Pit
-    create_square((0.64, 0.76, 0.95), ((19, 7), (24, 7), (24, 12), (19, 12)))  # Azul Breeze
-    create_square((1, 1, 1), ((19, 13), (24, 13), (24, 18), (19, 18)))  # Branco
-    create_square((0.64, 0.76, 0.95), ((19, 19), (24, 19), (24, 24), (19, 24)))  # Azul Breeza
+    create_square((colorPit), ((19, 1), (24, 1), (24, 6), (19, 6)))  # Preto Pit
+    create_square((colorBreeze), ((19, 7), (24, 7), (24, 12), (19, 12)))  # Azul Breeze
+    create_square((colorWhite), ((19, 13), (24, 13), (24, 18), (19, 18)))  # Branco
+    create_square((colorBreeze), ((19, 19), (24, 19), (24, 24), (19, 24)))  # Azul Breeza
 
 
 
